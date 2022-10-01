e3a5ae9331046d1b49434e32221de0b4a554d688
polyakhtar <poly09876pm@gmail.com>
2022-10-01 12:25:24 +0600

    header correction

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div class="header">
+            <h1>Abslolut Fitness Club</h1>
+        </div>
+    );
+};
+
+export default Header;
